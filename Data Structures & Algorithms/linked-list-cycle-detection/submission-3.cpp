82c915f2226d51f3db28515b21b10d5cdef6dc9c
essiarab <essiarab@users.noreply.github.com>
2026-04-09 15:54:32 +0200

    Add: linked-list-cycle-detection - submission-3

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-3.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        if((head== nullptr) || (head->next == nullptr))
+            return false;
+
+        ListNode* turtle;
+        ListNode* haze;
+        turtle = head;
+        haze = head->next;
+        
+        while((haze!=nullptr) && ((haze->next) != nullptr)) {
+            if(turtle == haze) {
+                return true;
+            }
+            turtle= turtle->next;
+            haze=haze->next->next;
+        }
+        return false;
+        
+    }
+};
